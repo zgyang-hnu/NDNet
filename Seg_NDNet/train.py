3d8a197b1afee8dc02a9908773b7e4c90d91595c
zgyang-hnu <52980101+zgyang-hnu@users.noreply.github.com>
2019-08-05 16:44:33 -0400

    Update train.py

--- Seg_NDNet/train.py
+++ Seg_NDNet/train.py
@@ -13,7 +13,7 @@ import numpy as np
 import torch
 from torch.utils import data
 import torch.nn as nn
-import torch.distributed as dist
+
 import torch.backends.cudnn as cudnn
 import torchvision.transforms as transforms
 
